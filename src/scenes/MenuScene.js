4219d1976041b919cac147ca25e9c3785d32aa72
Peter Mühleder <peter.muehleder@uni-leipzig.de>
2018-05-14 10:41:26 +0200

    menuscene: directly add image as physics object

--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -6,20 +6,17 @@ export default class extends Phaser.Scene {
     } 
 
     create () {
-        // put apple to the center of the game field
+        // add bouncy apple
         const x = this.sys.game.config.width / 2
         const y = this.sys.game.config.height / 2
-        this.apple = this.add.image(x, y, 'apple')
-
-        this.physics.world.enable(this.apple);
-
-        this.apple.body.setCollideWorldBounds(true).setBounce(1, 1).setVelocity(100,200)
+        this.apple = this.physics.add.image(x, y, 'apple')
+        this.apple.body.setCollideWorldBounds(true).setBounce(1, 1).setVelocity(100, 200)
     }
 
     update (time, delta) {
         if (this.apple.body.velocity.x > 0)
-            this.apple.rotation += delta/1000
+            this.apple.rotation += delta / 1000
         else
-            this.apple.rotation -= delta/1000
+            this.apple.rotation -= delta / 1000
     }
 }
